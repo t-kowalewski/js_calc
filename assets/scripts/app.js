daa131107c3d204ec0559cc740b3352844dc4742
t-kowalewski <wkowalewski@outlook.com>
2021-01-08 23:33:16 +0100

    add app.js; hardcode funct.

---
+++ assets/scripts/app.js
@@ -0,0 +1,12 @@
+// Initial Data
+const defaultResult = 0
+let currentResult = defaultResult;
+
+// Calculation Logic
+currentResult = currentResult + 10;
+
+// Calculation Description
+let calcDescription = '0 + 10';
+
+// Output Result
+outputResult(calcDescription, currentResult);
--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -4,10 +4,10 @@ const subtractBtn = document.getElementById('btn-subtract');
 const multiplyBtn = document.getElementById('btn-multiply');
 const divideBtn = document.getElementById('btn-divide');
 
-const currentResultOutput = document.getElementById('current-result');
 const currentCalculationOutput = document.getElementById('current-calculation');
+const currentResultOutput = document.getElementById('current-result');
 
-function outputResult(result, text) {
-  currentResultOutput.textContent = result;
+function outputResult(text, result) {
   currentCalculationOutput.textContent = text;
+  currentResultOutput.textContent = result;
 }
