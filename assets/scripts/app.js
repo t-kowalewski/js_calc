7f8ba7c3c0af0ff1271509e5ac199335b1dd0483
t-kowalewski <wkowalewski@outlook.com>
2021-01-09 23:38:31 +0100

    add additional calc logic and event listeners

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,10 +4,35 @@ let currentResult = defaultResult;
 
 // Event Listenteners - Calc Buttons
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
 
 // Calculation Logic - Functions
 function add() {
-  const calcDescription = `${currentResult} + ${userInput.value}`;
-  currentResult += userInput.valueAsNumber;
+  const enteredNumber = userInput.valueAsNumber;
+  const calcDescription = `${currentResult} + ${enteredNumber}`;
+  currentResult += enteredNumber;
+  outputResult(calcDescription, currentResult);
+}
+
+function subtract() {
+  const enteredNumber = userInput.valueAsNumber;
+  const calcDescription = `${currentResult} - ${enteredNumber}`;
+  currentResult -= enteredNumber;
+  outputResult(calcDescription, currentResult);
+}
+
+function multiply() {
+  const enteredNumber = userInput.valueAsNumber;
+  const calcDescription = `${currentResult} * ${enteredNumber}`;
+  currentResult *= enteredNumber;
+  outputResult(calcDescription, currentResult);
+}
+
+function divide() {
+  const enteredNumber = userInput.valueAsNumber;
+  const calcDescription = `${currentResult} / ${enteredNumber}`;
+  currentResult /= enteredNumber;
   outputResult(calcDescription, currentResult);
 }
