1ddd70904a354310b08c9faf93521c8914345a3d
t-kowalewski <wkowalewski@outlook.com>
2021-01-10 15:24:55 +0100

    small refactor for getUserInput

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -10,34 +10,38 @@ multiplyBtn.addEventListener('click', multiply);
 divideBtn.addEventListener('click', divide);
 
 // Calculation Output & Main Logic - Functions
+function getUserInput() {
+  return userInput.valueAsNumber;
+}
+
 function writeLogAndResult(originalResult, operator, newNumber) {
   const calcDescription = `${originalResult} ${operator} ${newNumber}`;
   outputResult(calcDescription, currentResult);
 }
 
 function add() {
-  const enteredNumber = userInput.valueAsNumber;
+  const enteredNumber = getUserInput();
   const initResult = currentResult;
   currentResult += enteredNumber;
   writeLogAndResult(initResult, '+', enteredNumber);
 }
 
 function subtract() {
-  const enteredNumber = userInput.valueAsNumber;
+  const enteredNumber = getUserInput();
   const initResult = currentResult;
   currentResult -= enteredNumber;
   writeLogAndResult(initResult, '-', enteredNumber);
 }
 
 function multiply() {
-  const enteredNumber = userInput.valueAsNumber;
+  const enteredNumber = getUserInput();
   const initResult = currentResult;
   currentResult *= enteredNumber;
   writeLogAndResult(initResult, '*', enteredNumber);
 }
 
 function divide() {
-  const enteredNumber = userInput.valueAsNumber;
+  const enteredNumber = getUserInput();
   const initResult = currentResult;
   currentResult /= enteredNumber;
   writeLogAndResult(initResult, '/', enteredNumber);
