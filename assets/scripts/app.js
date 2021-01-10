d0f45f7c367fb652ef5542e507189ef528409047
t-kowalewski <wkowalewski@outlook.com>
2021-01-10 18:56:52 +0100

    add logging functionality

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,7 @@
 // Initial Data
 const defaultResult = 0
 let currentResult = defaultResult;
+let logEntries = [];
 
 // Event Listenteners - Calc Buttons
 clearBtn.addEventListener('click', clear);
@@ -9,42 +10,63 @@ subtractBtn.addEventListener('click', subtract);
 multiplyBtn.addEventListener('click', multiply);
 divideBtn.addEventListener('click', divide);
 
-// Calculation Output & Main Logic - Functions
+// Functions
+// Calculation Output & Logging Logic
 function getUserInput() {
   return userInput.valueAsNumber;
 }
 
-function writeLogAndResult(originalResult, operator, newNumber) {
+function writeDescriptAndResult(originalResult, operator, newNumber) {
   const calcDescription = `${originalResult} ${operator} ${newNumber}`;
   outputResult(calcDescription, currentResult);
 }
 
+function addLogEntry(operation, num1, num2) {
+  const logEntrie = {
+    operation: operation,
+    num1: num1,
+    num2: num2,
+    result: currentResult
+  }
+  logEntries.push(logEntrie);
+  // console.log(logEntries);
+}
+
+// Calc Operations & Clear
 function add() {
   const enteredNumber = getUserInput();
   const initResult = currentResult;
   currentResult += enteredNumber;
-  writeLogAndResult(initResult, '+', enteredNumber);
+  writeDescriptAndResult(initResult, '+', enteredNumber);
+  
+  addLogEntry('ADD', initResult, enteredNumber);
 }
 
 function subtract() {
   const enteredNumber = getUserInput();
   const initResult = currentResult;
   currentResult -= enteredNumber;
-  writeLogAndResult(initResult, '-', enteredNumber);
+  writeDescriptAndResult(initResult, '-', enteredNumber);
+
+  addLogEntry('SUBTRACT', initResult, enteredNumber);
 }
 
 function multiply() {
   const enteredNumber = getUserInput();
   const initResult = currentResult;
   currentResult *= enteredNumber;
-  writeLogAndResult(initResult, '*', enteredNumber);
+  writeDescriptAndResult(initResult, '*', enteredNumber);
+
+  addLogEntry('MULTIPLY', initResult, enteredNumber);
 }
 
 function divide() {
   const enteredNumber = getUserInput();
   const initResult = currentResult;
   currentResult /= enteredNumber;
-  writeLogAndResult(initResult, '/', enteredNumber);
+  writeDescriptAndResult(initResult, '/', enteredNumber);
+
+  addLogEntry('DIVIDE', initResult, enteredNumber);
 }
 
 function clear() {
